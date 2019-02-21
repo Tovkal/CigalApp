7aa4011e474ac0cbf57d72b454e7b7f5cfe52872
Andrés Pizá <Tovkal@gmail.com>
2019-02-21 15:00:01 +0100

    Implement buttons

--- CigalApp/ViewController.swift
+++ CigalApp/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import AVKit
+
+private enum File: String {
+    case loharas, atras
+}
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    private var player: AVAudioPlayer?
+
+    @IBAction func didTapLoHaras() {
+        playFile(.loharas)
     }
 
+    @IBAction func didTapAtras() {
+        playFile(.atras)
+    }
 
+    private func playFile(_ file: File) {
+        guard let url = Bundle.main.url(forResource: file.rawValue, withExtension: "mp3") else {
+            fatalError("Audio file not found \(file.rawValue)")
+        }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            player?.play()
+        } catch {
+            fatalError()
+        }
+    }
 }
-
